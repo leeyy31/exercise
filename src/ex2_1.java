f85f3f42b0f0f4234bfd3938c761b5bcba29a9c3
Yujeong Lee <leeyy31@gmail.com>
2022-06-16 00:00:38 +0900

    자바 단축키

---
+++ src/ex2_1.java
@@ -0,0 +1,19 @@
+
+public class ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+alt+down 행단위 복사
+		// alt+shift+a 컬럼 편집 모드(토글)
+		// print() - 출력 후에 줄바꿈을 안함
+		// println() - 출력 후에 줄바꿈
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println(5+3);
+		System.out.println(5-3); 
+		System.out.println(5*3);
+		System.out.println(5/3);
+	}
+
+}
