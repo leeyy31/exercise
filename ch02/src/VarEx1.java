d7598c3f7ed3a4b587efbea8023057b5c2394f2d
Yujeong Lee <leeyy31@gmail.com>
2022-06-16 22:52:36 +0900

    변수의 활용

---
+++ ch02/src/VarEx1.java
@@ -0,0 +1,15 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("6+3"); // 문자그대로 출력
+		int x=5,y=2; //변수 활용의 장점
+		System.out.println(x+y); 
+		System.out.println(x-y); 
+		System.out.println(x/y); 
+		System.out.println(x*y); 
+
+	}
+
+}
